78392ca906a3baf287ee8b5f2b22f5d4c21f6c54
Ahmed Kaddoura <akadd8@gmail.com>
2021-03-06 14:36:43 -0800

    Implement header props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
     <div className="container">
-      <Header />
+      <Header title='Sharespace' />
     </div>
   );
 }
 
-export default App;
+export default App
