6a0a658f7acd6d78e87628a0280bb712e9081a82
Ahmed Kaddoura <akadd8@gmail.com>
2021-03-06 16:10:40 -0800

    Add day to header

--- src/components/Scheduler.jsx
+++ src/components/Scheduler.jsx
@@ -3,25 +3,25 @@ import * as dateFns from "date-fns";
 
 class Scheduler extends React.Component {
     state = {
-        currentMonth: new Date(),
+        currentDay: new Date(),
         selectedDate: new Date()
     };
 
     renderHeader() {
-        const dateFormat = "MMMM yyyy";
+        const dateFormat = "MMMM d, yyyy";
         return (
           <div className="header row flex-middle">
             <div className="col col-start">
-              <div className="icon" onClick={this.prevMonth}>
+              <div className="icon" onClick={this.prevDay}>
                 chevron_left
               </div>
             </div>
             <div className="col col-center">
               <span>
-                {dateFns.format(this.state.currentMonth, dateFormat)}
+                {dateFns.format(this.state.currentDay, dateFormat)}
               </span>
             </div>
-            <div className="col col-end" onClick={this.nextMonth}>
+            <div className="col col-end" onClick={this.nextDay}>
               <div className="icon">chevron_right</div>
             </div>
           </div>
@@ -32,8 +32,18 @@ class Scheduler extends React.Component {
     renderCells() {}
 
     onDateClick = day => {}
-    nextMonth = () => {}
-    prevMonth = () => {}
+    
+    nextDay = () => {
+        this.setState({
+            currentDay: dateFns.addDays(this.state.currentDay, 1)
+        });
+    };
+    
+    prevDay = () => {
+        this.setState({
+            currentDay: dateFns.subDays(this.state.currentDay, 1)
+        });
+    };
     
     render() {
         return (
